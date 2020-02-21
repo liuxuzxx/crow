1c4f80107094071753f1e31aa5edf557e455ff9a
liuxu <928525166@qq.com>
2020-02-21 12:20:04 +0800

    增加一些事件的操作，为截取视频做好准备

--- src/antd/video/VideoPlayerComponent.js
+++ src/antd/video/VideoPlayerComponent.js
@@ -15,17 +15,28 @@ class VideoPlayerComponent extends React.Component {
         console.log('VideoPlayerComponent Component is created!');
     }
 
+    ref = player => {
+        this.player = player
+    };
+
+    handlePause = () => {
+        console.log('onPause')
+        console.log(`查看时间:${this.player.getCurrentTime()}`)
+    };
+
     render() {
         let url = REMOTE_SERVER_URL + "/v1/api/rattrap/video/micro-video";
         return (
             <ReactPlayer
                 className='react-player'
+                ref={this.ref}
                 url={url}
                 width='50%'
                 height='50%'
                 playing={false}
                 playbackRate={10.0}
                 controls={true}
+                onPause={this.handlePause}
             />
         );
     }
