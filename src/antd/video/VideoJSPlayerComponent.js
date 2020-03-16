cfe84e15bee18f0d262263ca2fe1d74be73127ea
liuxu <928525166@qq.com>
2020-03-16 19:01:52 +0800

    准备放弃缩略图的展示，直接倍速播放就行，其实没有那么重要

--- src/antd/video/VideoJSPlayerComponent.js
+++ src/antd/video/VideoJSPlayerComponent.js
@@ -7,8 +7,6 @@
 import React from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
-import {REMOTE_SERVER_URL} from "../../config/RemoteRestConfig";
-import vttThumbnails from "videojs-vtt-thumbnails";
 
 class VideoJSPlayerComponent extends React.Component {
 
@@ -19,21 +17,16 @@ class VideoJSPlayerComponent extends React.Component {
             controls: true,
             playbackRates: [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 10.0],
             plugins: {
-                vttThumbnails: {
-                    src: REMOTE_SERVER_URL + "/api/rattrap/video/video-file/" + 1 + "/vtt-file",
-                    width: 120,
-                    height: 90,
-                },
             },
         };
     }
 
     componentDidMount() {
-        videojs.registerPlugin('vttThumbnails', vttThumbnails);
         let properties = {...this.props, ...this.defaultProperties};
         this.player = videojs(this.videoNode, properties, function onPlayerReady() {
             console.log(`准备好播放视频了!!!!!!`);
         });
+        console.log(this.player);
     }
 
     componentWillUnmount() {
